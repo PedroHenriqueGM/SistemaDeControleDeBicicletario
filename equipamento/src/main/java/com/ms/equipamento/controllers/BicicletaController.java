41aabde7e6db49bdf09417d85402e317a6594e1d
Pedro Magalhaes <phmagalhaes013@gmail.com>
2025-11-10 22:57:11 -0300

    Criando estrutura do projeto

---
+++ equipamento/src/main/java/com/ms/equipamento/controllers/BicicletaController.java
@@ -0,0 +1,12 @@
+package com.ms.equipamento.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bicicleta")
+public class BicicletaController {
+
+
+}
---
+++ equipamento/src/main/java/com/ms/equipamento/services/BicicletaService.java
@@ -0,0 +1,5 @@
+package com.ms.equipamento.services;
+
+public class BicicletaService {
+
+}

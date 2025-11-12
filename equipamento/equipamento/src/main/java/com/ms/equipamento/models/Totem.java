23170f2502af086f6bf85686553afcbe7aa5c49c
Raul Rodrigues <91432196+RaulRJ5@users.noreply.github.com>
2025-11-12 12:34:53 -0300

    Create Totem

---
+++ equipamento/equipamento/src/main/java/com/ms/equipamento/models/Totem.java
@@ -0,0 +1,41 @@
+package com.ms.equipamento.models;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "totens")
+public class Totem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String localizacao;
+
+    // usando o OneToMany pois o totem tem muitas trancas
+    @OneToMany(mappedBy = "totem", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tranca> trancas = new ArrayList<>();
+
+  
+    public Long getId() {
+        return id;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public List<Tranca> getTrancas() {
+        return trancas;
+    }
+
+    public void setTrancas(List<Tranca> trancas) {
+        this.trancas = trancas;
+    }
+}
